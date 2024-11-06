6f8ac0ea4875782a78be01ae124f734d27eb636b
Eva Maka <eva.maka@adamant-solutions.com>
2024-11-06 02:15:16 +0100

    patrimonio module with routing and lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'patrimonio', pathMatch: 'full' },
+  {
+    path: 'patrimonio',
+    loadChildren: () =>
+      import('./features/patrimonio/patrimonio.module').then(
+        (m) => m.PatrimonioModule
+      ),
+  },
+  {
+    path: 'contabilita',
+    loadChildren: () =>
+      import('./features/contabilita/contabilita.module').then(
+        (m) => m.ContabilitaModule
+      ),
+  },
+  {
+    path: 'anagrafica',
+    loadChildren: () =>
+      import('./features/anagrafica/anagrafica.module').then(
+        (m) => m.AnagraficaModule
+      ),
+  },
+  { path: '**', redirectTo: 'patrimonio' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './core/components/header/header.component';
 import { TabsComponent } from './core/components/header/tabs/tabs.component';
 import { FooterComponent } from './core/components/footer/footer.component';
