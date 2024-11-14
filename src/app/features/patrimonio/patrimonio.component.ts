121154d25c2e3ffa02683bd7a89081580ac8dc4a
Eva Maka <eva.maka@adamant-solutions.com>
2024-11-14 17:30:51 +0100

    patrimonio layout

--- src/app/features/patrimonio/patrimonio.component.ts
+++ src/app/features/patrimonio/patrimonio.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-patrimonio',
   templateUrl: './patrimonio.component.html',
-  styleUrls: ['./patrimonio.component.css']
+  styleUrls: ['./patrimonio.component.css'],
 })
 export class PatrimonioComponent {
-
+  // breadcrumbList = [{ label: 'Parent', link: '/parent' }];
+  breadcrumbList = [{ label: 'ERP - di Regione Puglia', link: '/' }];
+  // breadcrumbList = [];
 }
--- src/app/features/patrimonio/patrimonio.module.ts
+++ src/app/features/patrimonio/patrimonio.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { PatrimonioRoutingModule } from './patrimonio-routing.module';
 import { PatrimonioComponent } from './patrimonio.component';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    PatrimonioComponent
-  ],
-  imports: [
-    CommonModule,
-    PatrimonioRoutingModule
-  ]
+  declarations: [PatrimonioComponent],
+  imports: [CommonModule, PatrimonioRoutingModule, SharedModule],
 })
-export class PatrimonioModule { }
+export class PatrimonioModule {}
