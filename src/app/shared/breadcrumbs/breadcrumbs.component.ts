948e4506ae9b9fce2740173532dbbe4f36819da1
Eva Maka <eva.maka@adamant-solutions.com>
2024-11-14 17:27:13 +0100

    breadcrumbs

---
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumbs',
+  templateUrl: './breadcrumbs.component.html',
+  styleUrls: ['./breadcrumbs.component.css'],
+})
+export class BreadcrumbsComponent {
+  @Input() crumbsList!: Array<{ link: string; label: string }>;
+  @Input() currentPage!: string;
+}
