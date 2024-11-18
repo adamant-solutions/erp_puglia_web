ff15af770b517cb6c984fbce0926f8e2bf883e4d
Eva Maka <eva.maka@adamant-solutions.com>
2024-11-18 17:07:33 +0100

    anagrafica model

---
+++ src/app/core/models/anagrafica.model.ts
@@ -0,0 +1,52 @@
+export interface LuogoNascita {
+  comune: string;
+  provincia: string;
+  stato: string;
+}
+
+export interface Cittadino {
+  id: string;
+  codice_fiscale: string;
+  nome: string;
+  cognome: string;
+  data_nascita: string;
+  luogo_nascita: LuogoNascita;
+  sesso: string;
+  cittadinanza: string;
+}
+
+export interface Residenza {
+  indirizzo: string;
+  civico: number; // string ?
+  cap: string;
+  comune_residenza: string;
+  provincia_residenza: string;
+  stato_residenza: string;
+}
+
+export interface Contatti {
+  telefono: string;
+  email: string;
+  pec: string;
+}
+
+export interface DocumentiIdentita {
+  tipo_documento: string;
+  numero_documento: string;
+  data_emissione: string;
+  data_scadenza: string;
+  ente_emittente: string;
+}
+
+export interface AltriDettagli {
+  stato_civile: string;
+  data_ultimo_aggiornamento: string;
+}
+
+export interface Anagrafica {
+  cittadino: Cittadino;
+  residenza: Residenza;
+  contatti: Contatti;
+  documenti_identita: DocumentiIdentita[];
+  altri_dettagli: AltriDettagli;
+}
