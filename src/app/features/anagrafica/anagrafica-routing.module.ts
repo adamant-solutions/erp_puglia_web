c28bee72ba59945bc7262b0c6cfeb447315e464d
Eva Maka <eva.maka@adamant-solutions.com>
2024-11-21 20:08:46 +0100

    visualizza-anagrafica path

--- src/app/features/anagrafica/anagrafica-routing.module.ts
+++ src/app/features/anagrafica/anagrafica-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnagraficaComponent } from './anagrafica.component';
-import { anagraficaResolver } from 'src/app/core/resolvers/anagrafica.resolver';
+import {
+  anagraficaByIdResolver,
+  anagraficaResolver,
+} from 'src/app/core/resolvers/anagrafica.resolver';
+import { ViewAnagraficaComponent } from './view-anagrafica/view-anagrafica.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AnagraficaComponent,
     resolve: { anagraficaResolver: anagraficaResolver },
+  },
+  {
+    path: ':anagraficaId/visualizza-anagrafica',
+    component: ViewAnagraficaComponent,
+    resolve: { anagraficaByIdResolver },
   },
 ];
 
--- src/app/features/anagrafica/anagrafica.module.ts
+++ src/app/features/anagrafica/anagrafica.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { AnagraficaRoutingModule } from './anagrafica-routing.module';
 import { AnagraficaComponent } from './anagrafica.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ViewAnagraficaComponent } from './view-anagrafica/view-anagrafica.component';
 
 @NgModule({
-  declarations: [AnagraficaComponent],
-  imports: [CommonModule, AnagraficaRoutingModule, SharedModule],
+  declarations: [AnagraficaComponent, ViewAnagraficaComponent],
+  imports: [
+    CommonModule,
+    AnagraficaRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
 })
 export class AnagraficaModule {}
