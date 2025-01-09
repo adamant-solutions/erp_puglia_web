ae2b3f5c49060be7b1c55e984303bc08c109c5f8
Eva Maka <eva.maka@adamant-solutions.com>
2025-01-09 02:51:42 +0100

    patrimonio ts - data from resolver

--- src/app/features/patrimonio/patrimonio.component.ts
+++ src/app/features/patrimonio/patrimonio.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { Patrimonio } from 'src/app/core/models/patrimonio.model';
 
 @Component({
   selector: 'app-patrimonio',
   templateUrl: './patrimonio.component.html',
   styleUrls: ['./patrimonio.component.css'],
 })
-export class PatrimonioComponent {
+export class PatrimonioComponent implements OnInit {
   pageTitle: string = 'Patrimonio';
 
   breadcrumbList = [{ label: 'ERP - di Regione Puglia', link: '/' }];
+
+  patrimonioList: Patrimonio[] = [];
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.getPatrimonioList();
+  }
+
+  getPatrimonioList() {
+    // Load initial data from resolver
+    this.activatedRoute.data
+      .pipe(map((data) => data['patrimonioResolver']))
+      .subscribe((response) => {
+        console.log('Response from resolver:', response);
+        this.patrimonioList = response;
+      });
+  }
 }
