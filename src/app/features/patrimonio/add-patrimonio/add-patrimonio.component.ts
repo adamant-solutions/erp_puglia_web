f265e809ed3bf26dcafea67595605e36ebe4a570
Eva Maka <eva.maka@adamant-solutions.com>
2025-01-10 02:25:38 +0100

    generate add & edit patrimonio components

---
+++ src/app/features/patrimonio/add-patrimonio/add-patrimonio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-patrimonio',
+  templateUrl: './add-patrimonio.component.html',
+  styleUrls: ['./add-patrimonio.component.css']
+})
+export class AddPatrimonioComponent {
+
+}
---
+++ src/app/features/patrimonio/edit-patrimonio/edit-patrimonio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-patrimonio',
+  templateUrl: './edit-patrimonio.component.html',
+  styleUrls: ['./edit-patrimonio.component.css']
+})
+export class EditPatrimonioComponent {
+
+}
