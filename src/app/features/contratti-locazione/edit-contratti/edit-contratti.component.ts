b6bcb1bfa66738e056989a1461a1405d45085656
Igli Shahu <igli.shahu@topcallsrl.it>
2025-01-29 16:31:55 +0100

    edit contratti component

---
+++ src/app/features/contratti-locazione/edit-contratti/edit-contratti.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-contratti',
+  templateUrl: './edit-contratti.component.html',
+  styleUrls: ['./edit-contratti.component.css']
+})
+export class EditContrattiComponent {
+
+}
