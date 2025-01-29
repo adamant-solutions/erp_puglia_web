2de65b978390ba5042325ab52984095344c36bbb
Igli Shahu <igli.shahu@topcallsrl.it>
2025-01-29 15:04:43 +0100

    contratto model

---
+++ src/app/core/models/contratto.model.ts
@@ -0,0 +1,61 @@
+import { Contatti, DocumentiIdentita, LuogoNascita, Residenza } from "./anagrafica.model"
+
+export interface Contratti {
+    id: number
+    createDate: string
+    lastUpdateDate: string
+    dataInizio: string
+    dataFine: string
+    canoneMensile: number
+    statoContratto: StatoContratto;
+    descrizione: string
+    unitaImmobiliare: {
+      id: number;
+    };
+    intestatari: Intestatari[]
+    intestatariAttuali: IntestatariAttuali[]
+    intestatariStorici: any[]
+    documenti: any[] 
+  }
+  
+  
+  export type StatoContratto = 'ATTIVO' | 'SCADUTO' | 'DISDETTO' | 'ANNULLATO';
+  
+  
+  export interface Intestatari {
+    id: number
+    createDate: string
+    lastUpdateDate: string
+    intestatario: number
+    dataInizio: string
+    dataFine: any
+    motivoFine: any
+  }
+  
+  export interface IntestatariAttuali {
+    id: number
+    createDate: string
+    lastUpdateDate: string
+    cittadino: Cittadino
+  }
+  
+  export interface Cittadino {
+    id: number
+    createDate: string
+    lastUpdateDate: string
+    nome: string
+    cognome: string
+    codiceFiscale: string
+    genere: string
+    cittadinanza: string
+    dataDiNascita: string
+    residenza?: Residenza
+    contatti?: Contatti
+    luogo_nascita?: LuogoNascita
+    documenti_identita?: DocumentiIdentita[]
+  }
+
+  export interface ModelLight{
+    id: number;
+    descrizione: string;
+}
