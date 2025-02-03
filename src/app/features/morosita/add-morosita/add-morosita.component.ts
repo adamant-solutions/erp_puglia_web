1c95135f5e92a05474c6d4d62e5a77dfcfcf85a5
Igli Shahu <igli.shahu@topcallsrl.it>
2025-02-03 15:50:05 +0100

    add morosita component

---
+++ src/app/features/morosita/add-morosita/add-morosita.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-morosita',
+  templateUrl: './add-morosita.component.html',
+  styleUrls: ['./add-morosita.component.css']
+})
+export class AddMorositaComponent {
+
+}
