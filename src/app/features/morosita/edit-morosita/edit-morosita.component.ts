b71c751ce2dbaf5131fde5156a05c3d1dd0c78b4
Igli Shahu <igli.shahu@topcallsrl.it>
2025-02-03 15:50:15 +0100

    edit morosita component

---
+++ src/app/features/morosita/edit-morosita/edit-morosita.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-morosita',
+  templateUrl: './edit-morosita.component.html',
+  styleUrls: ['./edit-morosita.component.css']
+})
+export class EditMorositaComponent {
+
+}
