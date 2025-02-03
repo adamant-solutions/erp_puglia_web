a070ddf42f85c1f0692c64bdd3816a6f6c9d09b3
Igli Shahu <igli.shahu@topcallsrl.it>
2025-02-03 15:50:26 +0100

    view morosita component

---
+++ src/app/features/morosita/view-morosita/view-morosita.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-morosita',
+  templateUrl: './view-morosita.component.html',
+  styleUrls: ['./view-morosita.component.css']
+})
+export class ViewMorositaComponent {
+
+}
