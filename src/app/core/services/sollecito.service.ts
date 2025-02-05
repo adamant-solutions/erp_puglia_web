06ebf3612eed4c3947608b79bf97373b89091167
Igli Shahu <igli.shahu@topcallsrl.it>
2025-02-05 12:26:25 +0100

    sollecito service

---
+++ src/app/core/services/sollecito.service.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Sollecito } from '../models/sollecito.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SollecitoService {
+  constructor(
+    private http: HttpClient,
+    @Inject('morositaUrl') private morositaUrl: string
+  ) {}
+
+  getSollecitiByMorositaId(morositaId: number): Observable<Sollecito[]> {
+    return this.http.get<Sollecito[]>(`${this.morositaUrl}/${morositaId}/solleciti`);
+  }
+}
