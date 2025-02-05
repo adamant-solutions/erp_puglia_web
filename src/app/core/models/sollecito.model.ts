ced9f0d06ca698f2b63daa17d50dbe800833be9b
Igli Shahu <igli.shahu@topcallsrl.it>
2025-02-05 12:25:50 +0100

    sollecito model

---
+++ src/app/core/models/sollecito.model.ts
@@ -0,0 +1,15 @@
+export interface Sollecito  {
+    id: number
+    dataInvio: string
+    dataScadenza: string
+    tipoSollecito: string
+    esitoInvio: any
+    dataRisposta: any
+    esitoRisposta: any
+    costoSollecito: any
+    costoNotifica: any
+    costoLegale: any
+    dettaglioCosti: any
+    note: string
+    numeroProtocollo: any
+  }
