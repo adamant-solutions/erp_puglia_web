65a96e7c3817e90b449a4a95b664d4e8d2899c9f
Ronda Daia <ronda.daia@topcallsrl.it>
2025-02-07 16:19:05 +0100

    edit appalti

--- src/app/features/manutenzione/appalti/edit-appalti/edit-appalti.component.ts
+++ src/app/features/manutenzione/appalti/edit-appalti/edit-appalti.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Appalto, Imprese, StatoAppalto, TipoAppalto } from 'src/app/core/models/manutenzione.model';
+import { BootstrapService } from 'src/app/core/services/bootstrap-service.service';
+import { AppaltiService } from 'src/app/core/services/manutenzione-services/appalti.service';
 
 @Component({
   selector: 'app-edit-appalti',
@@ -6,5 +11,110 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-appalti.component.css']
 })
 export class EditAppaltiComponent {
+  pageTitle: string = 'Modifica Appalto';
+  breadcrumbList = [
+    { label: 'ERP - di Regione Puglia', link: '/' },
+    { label: 'Manutenzione', link: '/manutenzione' },
+    { label: 'Appalti', link: '/manutenzione/appalti' },
+  ];
+  appalto: Appalto = {
+    id: 0,
+    codiceCIG: '',
+    codiceCUP: '',
+    oggetto: '',
+    tipoAppalto: '',
+    stato: '',
+    importoBaseAsta: 0,
+    importoAggiudicazione: undefined,
+    dataPubblicazione: '',
+    dataScadenza: '',
+    dataAggiudicazione: '',
+    impresaAggiudicatariaId: 0,
+    pianoId: 0,
+    importo: 0,
+    note: ''
+  }
+
+  editForm!: FormGroup;
+  impreseList: Imprese[] = [];
+  statoList: StatoAppalto[] = [
+    StatoAppalto.IN_PROGRAMMAZIONE,
+    StatoAppalto.IN_PROGETTAZIONE,
+    StatoAppalto.BANDO_IN_CORSO,
+    StatoAppalto.VALUTAZIONE_OFFERTE,
+    StatoAppalto.AGGIUDICATO,
+    StatoAppalto.IN_ESECUZIONE,
+    StatoAppalto.SOSPESO,
+    StatoAppalto.COLLAUDATO,
+    StatoAppalto.CHIUSO,
+    StatoAppalto.ANNULLATO
+  ];
+
+  tipoAppaltoList: TipoAppalto[]=[
+    TipoAppalto.FORNITURE,
+    TipoAppalto.LAVORI,
+    TipoAppalto.SERVIZI
+  ]
+
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
+
+  constructor(private appSrc: AppaltiService,private bootstrap: BootstrapService,private fb: FormBuilder) { }
+  ngOnInit() {
+    this.route.data.subscribe(({ data , dataImprese}) => {
+      this.appalto = data
+      this.impreseList = [...dataImprese]
+      this.initForm();
+    })
+  }
+
+  initForm() {
+    this.editForm = this.fb.group({
+      id: this.appalto.id,
+      codiceCIG: [this.appalto.codiceCIG,[Validators.required,Validators.minLength(10)]],
+      codiceCUP: [this.appalto.codiceCUP,[Validators.required,Validators.minLength(15)]],
+      oggetto: [this.appalto.oggetto,[Validators.required,Validators.minLength(10),Validators.maxLength(500)]],
+      tipoAppalto: [this.appalto.tipoAppalto,[Validators.required]],
+      stato: [this.appalto.stato,[Validators.required]],
+      importoBaseAsta: [this.appalto.importoBaseAsta],
+      importoAggiudicazione: [this.appalto.importoAggiudicazione],
+      dataPubblicazione: [this.appalto.dataPubblicazione,[Validators.required]],
+      dataScadenza: [this.appalto.dataScadenza,[Validators.required]],
+      dataAggiudicazione: [this.appalto.dataAggiudicazione],
+      impresaAggiudicatariaId: [this.appalto.impresaAggiudicatariaId]
+    }); 
+
+  }  
+
+  onSubmit() {
+   // console.log('Form Submitted', this.editForm.value);
+    if (this.editForm.valid) {
+      const sendData = {
+        ...this.editForm.getRawValue(),
+        impresaAggiudicatariaId: +this.editForm.controls['impresaAggiudicatariaId'].value
+      }
+      console.log('Form Submitted', this.editForm.value);
+      this.appSrc.editAppalto(sendData).subscribe({
+        next: (res) => {
+          alert('Dati salvati con successo!');
+          this.router.navigate(['manutenzione/appalti/appalto-dettagli/' + this.appalto.id]);
+        },
+        error: (err) =>{
+          alert('Error!');
+        }
+      })
+  
+    }
+  }
+  
+  resetForm() {
+    this.editForm.reset(this.appalto);
+  }
+  
+  
+  indietro() {
+    this.router.navigate(['/manutenzione/appalti']);
+  }
 
 }
