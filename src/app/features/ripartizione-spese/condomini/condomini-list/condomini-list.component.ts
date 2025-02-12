56ece8d66657eb12817e53278dbc1b42121ac44c
Igli Shahu <igli.shahu@topcallsrl.it>
2025-02-12 18:01:53 +0100

    delete modal for condominio

--- src/app/features/ripartizione-spese/condomini/condomini-list/condomini-list.component.ts
+++ src/app/features/ripartizione-spese/condomini/condomini-list/condomini-list.component.ts
@@ -2,7 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { Condominio } from 'src/app/core/models/condominio.model';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { CondominioService } from 'src/app/core/services/ripartizione-spese/condominio.service';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-condomini-list',
@@ -25,9 +26,14 @@ export class CondominiListComponent {
   searchComuneParam = '';
   searchProvinciaParam = '';
 
-  constructor(private route: ActivatedRoute,  private router: Router) {}
+  condominioToDelete: Condominio | null = null;
+  deleteModal: any;
+  successModal: any;
+  constructor(private route: ActivatedRoute,  private router: Router,private condominioService: CondominioService) {}
 
   ngOnInit() {
+    this.deleteModal = new bootstrap.Modal(document.getElementById('deleteCondominioModal'));
+    this.successModal = new bootstrap.Modal(document.getElementById('deleteCondominioModal2'));
  
     this.route.queryParams.subscribe(params => {
       this.searchCodiceParam = params['codice'] || '';
@@ -65,6 +71,8 @@ export class CondominiListComponent {
       }
     });
   }
+
+  
 
   cercaCondomini(): void {
     this.currentPage = 0;
@@ -117,6 +125,57 @@ export class CondominiListComponent {
         size: this.pageSize
       }
     });
+  }
+
+  openDeleteModal(condominio: Condominio): void {
+    this.condominioToDelete = condominio;
+    this.deleteModal.show();
+  }
+
+  confirmDelete(): void {
+    if (this.condominioToDelete) {
+      this.condominioService.deleteCondominio(this.condominioToDelete.id).subscribe({
+        next: () => {
+         
+          this.deleteModal.hide();
+          
+       
+          this.successModal.show();
+  
+        
+          this.refreshList();
+        },
+        error: (error) => {
+          console.error('Error deleting condominio:', error);
+          this.deleteModal.hide();
+         
+        }
+      });
+    }
+  }
+  
+  refreshList(): void {
+ 
+    this.route.data.subscribe({
+      next: (data) => {
+        if (data['condomini']) {
+          const responseData = data['condomini'];
+          if (responseData.body) {
+            this.condominiList = responseData.body;
+            if (responseData.headers && responseData.headers.get('x-paging-totalrecordcount')) {
+              this.totalPages = parseInt(responseData.headers.get('x-paging-totalrecordcount') || '0');
+            }
+          }
+        }
+      }
+    });
+  }
+  
+  closeSuccessModal(): void {
+    this.successModal.hide();
+    this.condominioToDelete = null;
+
+    this.refreshList();
   }
 
 }
