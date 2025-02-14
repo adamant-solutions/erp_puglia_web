55b174e03c4883a7a21e2f43bb8fc61f367bb078
Igli Shahu <igli.shahu@topcallsrl.it>
2025-02-14 16:18:59 +0100

    add periodi error message

--- src/app/features/ripartizione-spese/condomini/condomini-list/add-condomini/add-condomini.component.ts
+++ src/app/features/ripartizione-spese/condomini/condomini-list/add-condomini/add-condomini.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/core/services/notification.service';
 import { CondominioService } from 'src/app/core/services/ripartizione-spese/condominio.service';
 
 @Component({
@@ -17,10 +18,12 @@ export class AddCondominiComponent {
   ];
   pageTitle = 'Nuovo condominio';
   submitted = false;
+  errorMsg: string = ''
   constructor(
     private fb: FormBuilder,
     private condominioService: CondominioService,
-    private router: Router
+    private router: Router,
+    private notificationService:NotificationService
   ) {
     this.addForm = this.fb.group({
       codice: ['', Validators.required],
@@ -46,12 +49,34 @@ export class AddCondominiComponent {
       this.condominioService.createCondominio(this.addForm.value)
         .subscribe({
           next: () => {
+            this.notificationService.addNotification({
+              message: 'Condominio è stato salvato con successo!',
+              type: 'success',
+              timeout: 3000,
+            });
             this.router.navigate(['ripartizione-spese/condomini']);
           },
           error: (error) => {
-            console.error('Error creating condominio:', error);
+            this.notificationService.addNotification({
+              message: this.handleError(error.error),
+              type: 'error',
+              timeout: 5000,
+            });
           }
         });
+    }
+  }
+
+  private handleError(error: any) : string{
+    switch (error.status) {
+      case 400:
+        return this.errorMsg = 'Dati non validi. Controlla i campi obbligatori.';
+      case 422:
+        return this.errorMsg = 'Dati non validi o condominio già esistente.';
+      case 500:
+        return this.errorMsg = error.message;
+      default:
+        return this.errorMsg = 'Errore durante il salvataggio del condominio.';
     }
   }
 
