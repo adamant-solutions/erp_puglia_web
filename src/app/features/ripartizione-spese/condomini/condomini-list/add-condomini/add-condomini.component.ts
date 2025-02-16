e5078907e67b1c8d424165c7eff507d7e35081a5
Igli Shahu <igli.shahu@topcallsrl.it>
2025-02-16 17:06:15 +0100

    prevent letters at CAP (nuovo condominio)

--- src/app/features/ripartizione-spese/condomini/condomini-list/add-condomini/add-condomini.component.ts
+++ src/app/features/ripartizione-spese/condomini/condomini-list/add-condomini/add-condomini.component.ts
@@ -357,6 +357,15 @@ export class AddCondominiComponent implements OnInit {
     }
   }
 
+  onCapKeyPress(event: KeyboardEvent): boolean {
+  
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
+
   indietro(): void {
     this.router.navigate(['ripartizione-spese/condomini']);
   }
