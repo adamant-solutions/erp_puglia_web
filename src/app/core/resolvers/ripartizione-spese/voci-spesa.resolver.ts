01e8d9bca772aa585cbd546a636ae08ba8f56d59
Igli Shahu <igli.shahu@topcallsrl.it>
2025-02-18 16:52:29 +0100

    search voci-spesa

--- src/app/core/resolvers/ripartizione-spese/voci-spesa.resolver.ts
+++ src/app/core/resolvers/ripartizione-spese/voci-spesa.resolver.ts
@@ -1,37 +1,29 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { ResolveFn } from '@angular/router';
 import { HttpResponse } from '@angular/common/http';
-import { VoceSpesaDTO,VoceSpesaSearchParams } from '../../models/voce-spesa.model';
+import { VoceSpesaDTO } from '../../models/voce-spesa.model';
 import { VoceSpesaService } from '../../services/ripartizione-spese/voce-spesa.service';
 import { PeriodoLight } from '../../models/periodi-gestione.model';
-import { Observable } from 'rxjs';
 
 export const vociSpesaResolver: ResolveFn<HttpResponse<VoceSpesaDTO[]>> = (
-    route,
-    state,
-    voceSpesaService: VoceSpesaService = inject(VoceSpesaService)
-  ) => {
-    const pagina = route.queryParams['pagina'] ? +route.queryParams['pagina'] : 0;
-    const size = route.queryParams['size'] ? +route.queryParams['size'] : 10;
-    
-    const searchParams: VoceSpesaSearchParams = {
-      descrizione: route.queryParams['descrizione'],
-      tipoSpesa: route.queryParams['tipoSpesa'],
-      importoMinPreventivo: route.queryParams['importoMinPreventivo'] ? +route.queryParams['importoMinPreventivo'] : undefined,
-      importoMaxPreventivo: route.queryParams['importoMaxPreventivo'] ? +route.queryParams['importoMaxPreventivo'] : undefined,
-      importoMinConsuntivo: route.queryParams['importoMinConsuntivo'] ? +route.queryParams['importoMinConsuntivo'] : undefined,
-      importoMaxConsuntivo: route.queryParams['importoMaxConsuntivo'] ? +route.queryParams['importoMaxConsuntivo'] : undefined,
-      note: route.queryParams['note'],
-      periodoId: route.queryParams['periodoId'] ? +route.queryParams['periodoId'] : undefined
-    };
-  
-    return voceSpesaService.getVociSpesa(pagina, size, searchParams);
+  route,
+  state,
+  voceSpesaService: VoceSpesaService = inject(VoceSpesaService)
+) => {
+  const searchParams = {
+    page: route.queryParams['pagina'] ? +route.queryParams['pagina'] : 0,
+ 
+    descrizione: route.queryParams['descrizione'],
+    periodoId: route.queryParams['periodoId'] ? route.queryParams['periodoId'] : ""
   };
-  
-  export const periodiResolver: ResolveFn<PeriodoLight[]> = (
-    route,
-    state,
-    voceSpesaService: VoceSpesaService = inject(VoceSpesaService)
-  ) => {
-    return voceSpesaService.getPeriodi();
-  };
+
+  return voceSpesaService.getVociSpesa(searchParams);
+};
+
+export const periodiResolver: ResolveFn<PeriodoLight[]> = (
+  route,
+  state,
+  voceSpesaService: VoceSpesaService = inject(VoceSpesaService)
+) => {
+  return voceSpesaService.getPeriodi();
+};
