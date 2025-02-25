4a71c79b3db7628a031b152f4c5756ec389ce26c
Ronda Daia <ronda.daia@topcallsrl.it>
2025-02-25 18:57:40 +0100

    registrazione contabile - part I

--- src/app/features/contabilita/contabilita-contratti/contabilita-contratti-routing.module.ts
+++ src/app/features/contabilita/contabilita-contratti/contabilita-contratti-routing.module.ts
@@ -3,13 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrazioniComponent } from './registrazioni/registrazioni.component';
 import { registrazioniContabileResolver } from 'src/app/core/resolvers/contabilita-resolvers/registrazioni-contabile.resolver';
 import { contrattiLightResolver } from 'src/app/core/resolvers/morosita.resolver';
+import { AddRegistrazioneComponent } from './add-registrazione/add-registrazione.component';
+import { pianoDeiContiparentResolver } from 'src/app/core/resolvers/contabilita-resolvers/piano-dei-conti.resolver';
 
 const routes: Routes = [{
   path: '',
   component: RegistrazioniComponent,
   resolve: { data: registrazioniContabileResolver , contrattiLightResolver},
   runGuardsAndResolvers: 'always',
-},];
+},{
+  path: 'nuova-registrazione',
+  component: AddRegistrazioneComponent,
+  resolve: { contrattiLightResolver,pianoDeiContiparentResolver},
+}];
 
 
 @NgModule({
--- src/app/features/contabilita/contabilita-contratti/contabilita-contratti.module.ts
+++ src/app/features/contabilita/contabilita-contratti/contabilita-contratti.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ContabilitaContrattiRoutingModule } from './contabilita-contratti-routing.module';
 import { RegistrazioniComponent } from './registrazioni/registrazioni.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AddRegistrazioneComponent } from './add-registrazione/add-registrazione.component';
 
 
 @NgModule({
   declarations: [
-    RegistrazioniComponent
+    RegistrazioniComponent,
+    AddRegistrazioneComponent
   ],
   imports: [
     CommonModule,
