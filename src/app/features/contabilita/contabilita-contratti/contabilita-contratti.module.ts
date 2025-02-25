edd0d4138cf86b6a04b79d5ab5706c0233c57958
Ronda Daia <ronda.daia@topcallsrl.it>
2025-02-25 11:52:36 +0100

    contabilita module, registrazione contabile service

---
+++ src/app/features/contabilita/contabilita-contratti/contabilita-contratti.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContabilitaContrattiRoutingModule } from './contabilita-contratti-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ContabilitaContrattiRoutingModule
+  ]
+})
+export class ContabilitaContrattiModule { }
