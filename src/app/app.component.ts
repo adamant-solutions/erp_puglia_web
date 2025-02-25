56cce6fe5e961f0d3637853c6c070c6de35a3412
Eva Maka <eva.maka@topcallsrl.it>
2025-02-25 04:31:26 +0100

    Layout change + entry overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'erp-puglia';
+
+  removeOverlay() {
+    const entryOverlay = document.querySelector('.entry-overlay');
+    if (entryOverlay) {
+      // entryOverlay.remove();
+      (entryOverlay as HTMLElement).style.display = 'none';
+    }
+  }
 }
