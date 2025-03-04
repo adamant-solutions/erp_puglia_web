49e49958329bd1321d0fc4cf08cf30964458bfe2
Eva Maka <eva.maka@topcallsrl.it>
2025-03-04 01:38:55 +0100

    overlay timer with localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'erp-puglia';
 
-  removeOverlay() {
-    const entryOverlay = document.querySelector('.entry-overlay');
-    if (entryOverlay) {
-      (entryOverlay as HTMLElement).style.opacity = '0';
-      (entryOverlay as HTMLElement).style.visibility = 'hidden';
+  entryOverlay!: HTMLElement | null;
+  private readonly OVERLAY_TIMEOUT = 1 * 60 * 1000; // 10 minutes in milliseconds
+
+  ngAfterViewInit() {
+    this.entryOverlay = document.querySelector('.entry-overlay');
+    this.checkOverlayVisibility();
+  }
+
+  checkOverlayVisibility() {
+    const lastDismissTime = localStorage.getItem('overlayDismissTime');
+    const currentTime = new Date().getTime();
+
+    if (
+      lastDismissTime &&
+      currentTime - parseInt(lastDismissTime, 10) < this.OVERLAY_TIMEOUT
+    ) {
+      this.hideOverlay();
     }
+  }
+
+  dismissOverlay() {
+    this.hideOverlay();
+    localStorage.setItem('overlayDismissTime', new Date().getTime().toString());
+  }
+
+  private hideOverlay() {
+    this.entryOverlay?.classList.add('hidden-overlay');
   }
 }
