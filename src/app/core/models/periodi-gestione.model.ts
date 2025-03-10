079ededc48315224f1e83ed492f529321d775ca3
Eva Maka <eva.maka@topcallsrl.it>
2025-03-10 16:44:42 +0100

    Periodi di Gestione - add Condominio indirizzo + comune + provincia

--- src/app/core/models/periodi-gestione.model.ts
+++ src/app/core/models/periodi-gestione.model.ts
@@ -1,15 +1,18 @@
 export interface PeriodiGestione {
-    id: number
-    dataInizio: string
-    dataFine: string
-    stato: string
-    condominioId: number
-    note: string
-    version: number
-  }
-  
-  export interface PeriodoLight {
-    id: number;
-    anno: number;
-    descrizione: string;
-  }
+  id: number;
+  indirizzoCondominio?: string;
+  comuneCondominio?: string;
+  provinciaCondominio?: string;
+  dataInizio: string;
+  dataFine: string;
+  stato: string;
+  condominioId: number;
+  note: string;
+  version: number;
+}
+
+export interface PeriodoLight {
+  id: number;
+  anno: number;
+  descrizione: string;
+}
